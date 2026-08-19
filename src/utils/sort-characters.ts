44744b1fe295f1759e626b9dc314c727a4850cf6
Miguel Barcellos <miguew.png@gmail.com>
2026-08-19 11:40:13 -0300

    chore: remove redundant JSDoc comments

--- src/utils/sort-characters.ts
+++ src/utils/sort-characters.ts
@@ -1,8 +1,5 @@
 import { Character } from '@/types/rick-and-morty'
 
-/**
- * Sorts an array of characters alphabetically by name without mutating the original array.
- */
 export function sortCharactersByName(characters: Character[]): Character[] {
   return [...characters].sort((a, b) => a.name.localeCompare(b.name))
 }
