64e4e6c12c767b189e5802f2e6f146fd4674f7ed
Miguel Barcellos <miguew.png@gmail.com>
2026-08-18 21:46:59 -0300

    feat: define Rick & Morty API TypeScript types

---
+++ src/types/rick-and-morty.ts
@@ -0,0 +1,14 @@
+export type Episode = {
+  id: number
+  name: string
+  episode: string
+  characters: string[]
+}
+
+export type Character = {
+  id: number
+  name: string
+  status: string
+  species: string
+  image: string
+}
